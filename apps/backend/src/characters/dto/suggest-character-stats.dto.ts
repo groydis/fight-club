94a9ae055cd15a76c4528be318903ad4c10ab87c
Greyden <greydenscott@gmail.com>
2025-06-01 08:51:36 +1000

    fix/SuggestCharacterStatsDto

--- apps/backend/src/characters/dto/suggest-character-stats.dto.ts
+++ apps/backend/src/characters/dto/suggest-character-stats.dto.ts
@@ -1,11 +1,6 @@
 import { IsString, MinLength } from 'class-validator';
 
 export class SuggestCharacterStatsDto {
-  constructor(name: string, description: string) {
-    this.name = name;
-    this.description = description;
-  }
-
   @IsString()
   @MinLength(1)
   name: string;
