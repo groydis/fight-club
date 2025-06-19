755e222c83d6feeb02510a774871360c31757524
Greyden <greydenscott@gmail.com>
2025-06-19 20:00:48 +1000

    make healthz end point public

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './common/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor() {}
 
+  @Public()
   @Get('/healthz')
   getHealth(): string {
     return 'OK';
