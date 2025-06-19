3ff946e2345bc29a6d26120e95e5c5d18d9a52f9
Greyden <greydenscott@gmail.com>
2025-06-19 20:40:14 +1000

    switch to static site

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  target: 'static',
+  baseURL: '/',
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   components: [
