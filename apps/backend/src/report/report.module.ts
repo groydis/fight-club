63c88866ad0563ef30257ed07332204511c1dfe1
Greyden <greydenscott@gmail.com>
2025-06-21 18:23:38 +1000

    Fix broken module

--- apps/backend/src/report/report.module.ts
+++ apps/backend/src/report/report.module.ts
@@ -1,12 +1,14 @@
 // src/report/report.module.ts
 
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../services/prisma/prisma.service';
 import { CreateReportService } from './services/create-report.service';
 import { ReportController } from './report.controller';
+import { PrismaModule } from '../services/prisma/prisma.module';
+import { SupabaseModule } from '..//services/supabase/supabase.module';
 
 @Module({
+  imports: [PrismaModule, SupabaseModule],
   controllers: [ReportController],
-  providers: [CreateReportService, PrismaService],
+  providers: [CreateReportService],
 })
 export class ReportModule {}
